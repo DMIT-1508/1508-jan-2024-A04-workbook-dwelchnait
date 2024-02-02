b0c06041b177be0e0dfceb3612fa1b958a914b5a
Don Welch <dwelch@nait.ca>
2024-02-02 09:49:20 -0700

    demo on Check and Default constraints

--- MyCompany Create Tables.sql
+++ MyCompany Create Tables.sql
@@ -38,29 +38,49 @@ go
 --		the attribute level (the constraint is restricted to that attribute)
 --		at the table level (a constraint can use multiple attributes
 --							within the same database)
+
+--checks: compound check on AGE
+--        regular expression (pattern/regex) on Phone
 CREATE TABLE Customers
 (
 CustomerNumber int IDENTITY(1,1) NOT NULL
 	CONSTRAINT PK_Customers_CustomerNumber primary key clustered,
 LastName nvarchar(50) NOT NULL,
 FirstName nvarchar(20) NOT NULL,
-Phone varchar(12) NULL
+Phone char(12) NULL
+	CONSTRAINT CK_Customers_Phone CHECK (Phone like '[1-9][0-9][0-9][ -][1-9][0-9][0-9][ -][0-9][0-9][0-9][0-9]'),
+Age int NOT NULL
+	CONSTRAINT CK_Customers_Age CHECK (Age BETWEEN 18 and 120)
+	--CONSTRAINT CK_Customers_Age CHECK (Age >= 18 and Age <= 120)
 )
 go
 
+-- CurrentPrice must be 0 or greater (Domain check/business rule)
 CREATE TABLE Items
 (
 ItemNumber int IDENTITY(1,1) NOT NULL
 	CONSTRAINT PK_Items_ItemNumber primary key clustered,
 Description nvarchar(150) NOT NULL,
 CurrentPrice smallmoney NOT NULL
+	CONSTRAINT CK_Items_CurrentPrice CHECK (CurrentPrice >= 0.00)
 )
 go
 
+
+--table level check constraints can check multiple attributes of the SAME table
+--	BUT need to be code at the table level
+
+--a default constrait allows the attribute to have a value when a value
+--		was NOT supplied
+--on the ShippedDate date, I could default the date to the current date
+--getdate() is a sql method that goes to the operating system and obtains the
+--		current date
 CREATE TABLE Orders
 (
 OrderNumber int IDENTITY(1,1) NOT NULL,
 OrderDate datetime NOT NULL,
+ShippedDate datetime NOT NULL
+	CONSTRAINT DF_Orders_ShippedDate DEFAULT getdate(),
 SubTotal smallmoney NOT NULL,
 GST smallmoney NOT NULL,
 Total smallmoney NOT NULL,
@@ -68,9 +88,13 @@ CustomerNumber int NOT NULL
 	CONSTRAINT FK_OrdersCustomers_CustomerNumber 
 		foreign key (CustomerNumber)
 		references Customers(CustomerNumber),
-CONSTRAINT PK_Orders_OrderNumber primary key clustered (OrderNumber)
+CONSTRAINT PK_Orders_OrderNumber primary key clustered (OrderNumber),
+CONSTRAINT CK_Orders_OrderDateShippedDate CHECK (ShippedDate >= OrderDate) 
 )
 go
+
+--compound primary keys need to be coded at the table level
+--why they contain 2 or more attributes
 CREATE TABLE ItemsOnOrder
 (
 OrderNumber int NOT NULL
@@ -81,7 +105,8 @@ ItemNumber int NOT NULL
 	CONSTRAINT FK_ItemsOnOrderItems_ItemNumber 
 		foreign key (ItemNumber)
 		references Items(ItemNumber),
-Quantity int NOT NULL,
+Quantity int NOT NULL
+	CONSTRAINT DF_ItemsOnOrder DEFAULT 1,
 Price smallmoney NOT NULL,
 Amount smallmoney NOT NULL,
 CONSTRAINT PK_ItemsOnOrder_OrderNumberItemNumber 
