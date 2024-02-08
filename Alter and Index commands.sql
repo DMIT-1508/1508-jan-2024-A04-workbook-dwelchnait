545e95387bcc2b48dfa0806113dd35402a2b2a5a
Don Welch <dwelch@nait.ca>
2024-02-08 09:51:06 -0700

    Update Alter and Index commands.sql

--- Alter and Index commands.sql
+++ Alter and Index commands.sql
@@ -17,3 +17,20 @@ ALTER TABLE Customers ADD
 	CONSTRAINT DF_Customers_Province DEFAULT 'AB' for Province
 
 ALTER TABLE ItemsOnOrder DROP COLUMN Amount
+
+-- Indexes
+--normally applied to foreign keys
+Create nonclustered INDEX IX_OrdersCustomers_CustomerNumber
+		ON Orders(CustomerNumber)
+
+--how to remove an index
+DROP INDEX IX_OrdersCustomers_CustomerNumber on Orders
+
+--can one create an index on part of a compound key
+CREATE nonclustered INDEX IX_ItemsOnOrderItems_ItemNumber
+		ON ItemsOnOrder(ItemNumber)
+
+-- ensure your Item Description is unique
+-- do it with a constraint or index
+CREATE unique nonclustered INDEX IX_Items_Description
+		ON Items(Description)
